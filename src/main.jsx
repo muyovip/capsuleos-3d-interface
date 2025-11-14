5423549fe7ee4012ebd3e518189aa9cafa256ef4
muyovip <madebymuyo@gmail.com>
2025-11-14 17:16:54 -0600

    Remove duplicate imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(<App />)
